084afb43e4817788d6655eb27b3393a1c8148891
Razelle McCarrick <razelle@satyaorthopedic.com>
2017-11-26 01:07:37 +0000

    automatically backed up by learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -2,7 +2,7 @@
 def fizzbuzz(number)
   if (number / 3) == 1
     "Fizz"
-  elsif (number / 5) == 1
+  elsif (number / 5) == 1 || (number / 5) == 2 || (number / 5) == 3
     "Buzz"
   elsif (number / 3) == 5 || (number / 5) == 3
     "FizzBuzz"
