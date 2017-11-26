b4da4fb8f52a761d2f97c82f5ee8f33d88e67fbb
Razelle McCarrick <razelle@satyaorthopedic.com>
2017-11-26 00:58:28 +0000

    automatically backed up by learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,10 +1,10 @@
 
 def fizzbuzz(number)
-  if number = 3
+  if number / 3 == 1
     puts "Fizz"
-  elsif number = 5
+  elsif number / 5 = 1
     puts "Buzz"
-  elsif number = "15"
+  elsif number / 3 = 15 || number / 5 = 15
     puts "FizzBuzz"
   end
 end
